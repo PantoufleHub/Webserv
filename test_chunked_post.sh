566e8a5d4f4ee60dad81a5f5584b8cb685615a5a
Poid-Lourd <pcardin@student.42lausanne.ch>
2025-10-16 16:03:39 +0200

    setUploadStore updated to handle creation of directories

--- test_chunked_post.sh
+++ test_chunked_post.sh
@@ -2,11 +2,11 @@
 
 {
   echo -ne "POST /post/toto2.txt HTTP/1.1\r\n"
-  echo -ne "Host: localhost:8080\r\n"
+  echo -ne "Host: localhost:8081\r\n"
   echo -ne "Transfer-Encoding: chunked\r\n"
   echo -ne "Content-Type: text/plain\r\n"
   echo -ne "\r\n"
   echo -ne "5\r\nHello\r\n"
   echo -ne "4\r\n World\r\n"
   echo -ne "0\r\n\r\n"
-} | nc -q 1 localhost 8080
+} | nc -q 1 localhost 8081
